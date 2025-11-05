8ea5d4f643d1e4a436a87bfa1865c6e2ef35d34e
lia <239637186+liacott@users.noreply.github.com>
2025-11-05 03:15:21 -0500

    Add project files.

---
+++ src/MenuManager.h
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <algorithm>
+
+#define NOMINMAX
+#define WINDOWS32_LEAN_AND_MEAN
+#include <Windows.h>
+
+// Display main menu and handle input
+std::string display_main_menu()
+{
+	return "";
+}
+
+// Display difficult select menu and handle input
+std::string display_difficulty_select() 
+{
+	return "";
+}
---
+++ src/main.cpp
@@ -0,0 +1,43 @@
+﻿#include "APeregrinatorsTale.h"
+
+#include <functional>	// for functionn
+#include <memory>		// for make_shared & shared_ptr
+#include <utility>		// for forward
+
+#include <ftxui/util/warn_windows_macro.hpp>
+#include "ftxui/component/component_base.hpp" // for Component & Components
+#include "ftxui/component/component_options.hpp"  // for ButtonOption, CheckboxOption & MenuOption
+#include "ftxui/dom/elements.hpp" // for Element
+#include "ftxui/util/ref.hpp"  // for ConstRef, Ref, ConstStringRef, ConstStringListRef & StringRef
+
+#include <iostream>
+#include <algorithm>
+
+#define NOMINMAX
+#define WINDOWS32_LEAN_AND_MEAN
+#include <Windows.h>
+
+using namespace std;
+
+int main()
+{
+
+	// display_main_menu()
+
+	
+
+
+
+
+
+
+
+
+
+	auto screen = Screen::Create(Dimension::Full(), Dimension::Fit(document));
+	Render(screen, document);
+	screen.Print();
+
+	return 0;
+
+}
